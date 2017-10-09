5259f20fa71095439b83f02d4d5397ffea9f1d8e
ththth6543 <ththth6543@gmail.com>
2017-10-09 23:38:14 +0900

    Update b.c

--- b.c
+++ b.c
@@ -8,6 +8,12 @@ int main(void)
 	char **player = (char **)calloc(10, sizeof(char *));
 	int i, j, d, k,sum, p;
 	sum = 0;
+	FILE *score_board; // 파일 입출력
+
+	score_board = fopens("score.txt", "w");
+	
+	if ((score_board = fopens("score.txt", "w")) == NULL)
+		printf("오류 : 파일을 열 수 없습니다.\n"); // 오류메세지
 
 	for (k = 0; k < 10; k++)
 	{
@@ -20,18 +26,20 @@ int main(void)
 				break;
 		}
 	}
-	for (j=0; j<10; j++)
-		printf("%s", *(player+j));
 
-	for(p = 0; p < 10; p++)
+
+	/*for (j=0; j<10; j++) 입력됬는지 확인용
+		printf("%s", *(player+j)); */
+
+	for(p = 0; p < 10; p++) // 점수 계산
 		if (p != 9)//1~9 프레임
 		{
-			if (strlen(*(player + p)) == 2) // 스트라이크
+			if (*(*(player + p)) == 'x') // 스트라이크
 			{
-				if (strlen(*(player + p + 1)) == 2) // 스트라이크
+				if (*(*(player + p + 1)) == 'x') // 스트라이크
 				{
-					if (strlen(*(player + p + 2)) == 2) // 스트라이크
-						sum += 30
+					if (*(*(player + p + 2)) == 'x') // 스트라이크
+						sum += 30;
 					else if (*(*(player + p + 2) + 1) == '/') //스플릿
 						sum += 20 + atoi(*(player + p + 2));
 					else                                   // 그 외의 경우
@@ -42,33 +50,48 @@ int main(void)
 					if (*(*(player + p + 1) + 1) == '/') //스플릿
 						sum += 10 + atoi(*(player + p + 1));
 					else                         // (숫자,숫자)
-						sum += 10 + atoi(*(player + p + 1)) / 10;
+						sum += 10 + (atoi(*(player + p + 1)) / 10);
 				}
 			}
 			else if (*(*(player + p) + 1) == '/') // 스플릿
 			{
-				if (strlen(*(player + p + 1)) == 2) // 스트라이크
+				if (*(*(player + p + 1)) == 'x') // 스트라이크
 					sum += 20;
 				else if (*(*(player + p + 1) + 1) == '/') // 스플릿
 					sum += 10 + atoi(*(player + p + 1));
 				else                                  // 그 외
-					sum += 10 + atoi(*(player + p + 1)) / 10;
+					sum += 10 + (atoi(*(player + p + 1)) / 10);
 			}
 			else                            // (숫자,숫자)
 				sum += (atoi(*(player + p)) / 10) + (atoi(*(player + p)) % 10);
 		}
 		else // 10 프레임
 		{
-			if (strlen(*(player + p)) == 2) // 스트라이크
+			if (*(*(player + p)) == 'x') // 스트라이크
+			{
+				if (*(*(player + p) + 1) == 'x') // 스트라이크
+				{
+					if (*(*(player + p) + 2) == 'x') // 스트라이크
+						sum += 30;
+					else
+						sum += 20 + atoi(*(player + p)); // 스트라이크 외의 경우
+				}
+			}
+			else if (*(*(player + p) + 1) == '/') // 스플릿
+			{
+				if (*(*(player + p) + 2) == 'x') // 스트라이크
+					sum += 20;
+				else
+					sum += 10 + (((int) *(*(player + p) + 2)) - 48);
+					// 스트라이크 외의 경우
+			}
 		}
+	for (j = 0; j < 10; j++)
+		fprintf(score_board, "%s", *(player + j));
+	
+	fclose(score_board);
 
-
-
-	printf("%d", strlen(*(player + 1)));
-	printf("%c", *(*(player + 1) + 1));
-	printf("%d\n", atoi(*(player + 1 + 1)) );
-	if ( *(*(player + 1) + 1) == '/' )
-		printf("1");
+	printf("총 점수는 %d점 입니다.\n", sum);
 
 	for (d = 0; d < 10; d++)
 		free(player+d);
