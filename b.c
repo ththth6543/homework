6748d104b23022f5b2403c1182e8b5de9c000a3d
ththth6543 <ththth6543@gmail.com>
2017-10-09 17:29:06 +0900

    Update b.c

--- b.c
+++ b.c
@@ -5,33 +5,35 @@
 
 int main(void)
 {
-	char **player;
+	char **player = (char **)calloc(10, sizeof(char *));
 	int i, j, d;
 	i = 0;
-	player = (char **)calloc(10, sizeof(char));
 	
-	while (i<5)
+	while (i<10)
 	{
 		printf("입력 : ");
-		while (1)
+		while (i<10)
 		{
-			*(player + i) = (char *)calloc(strlen(player)+1, sizeof(char));
+			*(player + i) = (char *)calloc(4, sizeof(char));
 			
-			*(*(player + i) + 1) = getchar();
+			*(*(player + i)) = getchar();
 			
 			
 
-			if ((*(*(player + i) + 1) == '\n') || (*(*(player + i) + 1) == EOF))
+			if ((*(*(player + i)) == '\n') || (*(*(player + i)) == EOF))
 			{
-				player[i] = '\0';
+				*(player+i) = '\0';
 				break;
 			}
 		}
 		
 		i++;
 	}
-	for (j = 0; j<10; j++)
-		printf("%s", player[j]);
+	for (j = 0; j < 10; j++)
+	{
+		printf("%s", *(player)+j);
+		//printf("%d", atoi( *(*(player + j))) );
+	}
 
 	for (d = 0; d < 10; d++)
 		free(player+d);
